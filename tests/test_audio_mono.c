0acda27c073bd74f8adcc2e4c041fa5c06a9c113
holojello <hadrien.delattre@mail.com>
2026-08-19 11:56:35 +0200

    Add a test of audio (doesn't build yet)

---
+++ tests/test_audio_mono.c
@@ -0,0 +1,39 @@
+#include "audio.h"
+
+#include <assert.h>
+#include <math.h>
+
+void
+test_pcm_audio_load_mono(void)
+{
+    const char *filename = "tests/fixtures/mono_5_samples.wav";
+    const float expected[] = {
+        0.8f,
+        -0.2f,
+        0.0f,
+        0.5f,
+        -0.9f
+    };
+
+    const float tolerance = 0.0001f;
+
+    pcm_audio_t *audio = pcm_audio_load(filename);
+
+    assert(audio != NULL);
+
+    assert(audio->samplerate == 44100);
+    assert(audio->channels == 1);
+    assert(audio->frame_count == 5);
+
+    assert(audio->channels_data != NULL);
+    assert(audio->channels_data[0] != NULL);
+
+    for (size_t frame = 0; frame < audio->frame_count; ++frame) {
+        assert(
+            fabsf(audio->channels_data[0][frame] - expected[frame])
+            < tolerance
+        );
+    }
+
+    pcm_audio_destroy(audio);
+}
